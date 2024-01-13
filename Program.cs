1cf715ae6fb3a59bb8d630b2f3ddb280ba28b424
MinimumADHD <simone.cappe06@gmail.com>
2024-01-13 20:46:54 +0100

    Changing color of output text Commands should show console output.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ class Program
     {
         bool releaseRenewFlag = false;
 
-        Console.WriteLine("Press F8 to toggle ipconfig /release and /renew.");
+        Console.WriteLine("Press F8 to toggle ipconfig /release and /renew.", Console.ForegroundColor = ConsoleColor.DarkBlue);
 
         while (true)
         {
@@ -25,12 +25,12 @@ class Program
                 if (releaseRenewFlag)
                 {
                     ExecuteCommand("ipconfig /release");
-                    Console.WriteLine("ipconfig /release executed.");
+                    Console.WriteLine("ipconfig /release executed.", Console.ForegroundColor = ConsoleColor.Magenta);
                 }
                 else
                 {
                     ExecuteCommand("ipconfig /renew");
-                    Console.WriteLine("ipconfig /renew executed.");
+                    Console.WriteLine("ipconfig /renew executed.", Console.ForegroundColor = ConsoleColor.Magenta);
                 }
 
                 // Wait for the key release to avoid multiple executions
@@ -41,13 +41,14 @@ class Program
 
     static void ExecuteCommand(string command)
     {
+        // this code hides (by default) the terminal logs, which is NOT what I need
         ProcessStartInfo processStartInfo = new ProcessStartInfo
         {
             FileName = "cmd.exe",
             RedirectStandardInput = true,
             RedirectStandardOutput = true,
             CreateNoWindow = true,
-            UseShellExecute = false
+            //UseShellExecute = false
         };
 
         Process process = new Process { StartInfo = processStartInfo };
